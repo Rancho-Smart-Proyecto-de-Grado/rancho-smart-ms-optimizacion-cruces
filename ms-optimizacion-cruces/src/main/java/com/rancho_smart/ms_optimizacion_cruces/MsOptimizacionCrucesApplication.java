c4c2ee0761ea276caa6b6da32227e912e9978e5d
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 03:00:59 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ ms-optimizacion-cruces/src/main/java/com/rancho_smart/ms_optimizacion_cruces/MsOptimizacionCrucesApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_optimizacion_cruces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOptimizacionCrucesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOptimizacionCrucesApplication.class, args);
+	}
+
+}
---
+++ ms-optimizacion-cruces/src/test/java/com/rancho_smart/ms_optimizacion_cruces/MsOptimizacionCrucesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.ms_optimizacion_cruces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOptimizacionCrucesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
