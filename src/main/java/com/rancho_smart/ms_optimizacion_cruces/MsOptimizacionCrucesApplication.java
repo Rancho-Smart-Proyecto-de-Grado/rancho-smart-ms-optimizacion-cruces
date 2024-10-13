3d3b77572d8cfc4089b2dd64d9292a5e8597810d
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-13 06:59:45 -0500

    :wrench: chore: Configuration Server + DB Creation + Environment Variables + Eureka Config.

--- src/main/java/com/rancho_smart/ms_optimizacion_cruces/MsOptimizacionCrucesApplication.java
+++ src/main/java/com/rancho_smart/ms_optimizacion_cruces/MsOptimizacionCrucesApplication.java
@@ -2,8 +2,10 @@ package com.rancho_smart.ms_optimizacion_cruces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsOptimizacionCrucesApplication {
 
 	public static void main(String[] args) {
